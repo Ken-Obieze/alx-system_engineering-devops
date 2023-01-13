98f655b2327ff54691b4d6020ee4756d2ebf66db
Ken-Obieze <kenneth.obieze@outlook.com>
2023-01-13 02:37:44 +0100

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # install flask
 
 exec {'flask':
-  ensure   => '2.1.0'
-  provider => 'pip3'
+#  ensure   => '2.1.0'
+#  provider => 'pip3'
+  command => 'usr/bin/apt-get python-3.8 -m pip3 install -y flask -v 2.1.0'
 }
