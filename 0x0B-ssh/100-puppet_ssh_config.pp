7ce0650bb1ff00c8eb34091c2ea140a31d41661b
Ken-Obieze <kenneth.obieze@outlook.com>
2023-02-01 04:52:16 +0100

    fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,15 @@
 #client config
 
-file_line { 'password auth':
-	path  => '/etc/ssh/ssh_config',
-	match => '^ PasswordAuthentication',
-	line  => ' PasswordAuthentication no',
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'set identity file':
-	path  => '/etc/ssh/ssh_config',
-	match => '^ IdentityFile',
-	line  => ' IdentityFile ~/.ssh/holberton',
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
